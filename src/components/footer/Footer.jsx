05be8101648594bd626819f4e83bd6d80c0b4077
Sanket <sanketudaypatil@gmail.com>
2023-05-29 10:54:53 +0530

    footer fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ import {
     FaLinkedin,
 } from "react-icons/fa";
 
-import ContentWrapper from "../contentWrapper/ContentWrapper";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper"
 
 import "./style.scss";
 
